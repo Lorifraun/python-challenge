853be5915412733ffc9f17f7f9442455cfde0083
lorifraun <lorifraun@live.com>
2021-03-27 19:17:01 -0600

    Adding and Updating

--- PyBank/mainbank.py
+++ PyBank/mainbank.py
@@ -1,59 +1,68 @@
 import os
 import csv
-'set the path'
-csvpath = os.path.join("Resources", "budget_data.csv")
-
-Total_months = 0
-Net_Total_amount = 0
 
 
+#set varibles
+total_months = []
+total_profit = []
+monthly_profit_change = []
 
-# = open("budget_data.csv","r")
-
-'get the list of all lines in the text file'
-lines = Bank_Data.readlines()
-
-#read the whole file line by line
-for i in range(0), len(lines))"
-    print lines [i]"
-
-import csv
-myfile = open("C:\\Users\\your_username\\Desktop\\example1.csv", "r")  # "r" means that we only want to read the file and get the information from the file, there are couple options
-csvfile = csv.reader(myfile)  # get rows in the file
-for row in csvfile:  # print all rows in the file
-     print row
-
-
-
-#open csv path
+#set path
+csvpath = os.path.join('C:\\Users\\lorif\\Demo\\python-challenge\\PyBank\\Resources\\budget_data.csv')
+#open csv file
 with open(csvpath) as csvfile:
-    #csv reader specifies delimiter and variable the holds contents
-    csvreader = csv.reader(csvfile, delimiter= ',')
-    
-    #read the header row first
-    csv_header = next(csvreader)
-    
 
+    #create csvreader variable to store budget.csv file
+    csvreader = csv.reader(csvfile, delimiter=",")
 
+    #skip header label
+    header = next(csvreader)
 
-
-    #read each row of data after the header
+    #loop thru the rows in the csvreader stored contents
     for row in csvreader:
-        total_months = total_months + 1
-        total_pl += int(row[1])
-      # in this loop I did sum of column 1 which is revenue in csv file and counted total months which is column 0 
-        changes_in_pl.append(float(row[1])
-        #date.append(row[0])
-        
-        
-        #in this loop I did total of difference between all row of column "Profit/Losses" and found total PL change. Also found out max PL change and min PL change. 
-    for i in range(len(changes_in_pl)):
-        #total_changes += changes_in_pl[i]
-        total_changes.append(changes_in_pl[i] - changes_in_pl[i-1])   
-    avg_changes = total_changes / len(changes_in_pl)
-        
-        
-        
-        print(row)
 
-'open file'
+        #split date to get month
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+        
+    #loop thru profits to get monthly change
+    for i in range(len(total_profit)-1):
+
+        #difference between two months
+        monthly_profit_change.append(total_profit[i+1] - total_profit[i])
+
+#calculate max and min monthly profit list
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
+
+#correlate max and min values to correct list and index. Use 1 to change to next month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
+
+#final statements
+print("Financial Analysis")
+print("------------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change), 2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+#open function to open the file "Final_Lori.txt"
+#(same directory)in append mode 
+#file1 = open("Final_Lori","a")
+#file1.close()
+
+#store its reference in the variable file1
+# and "Final_Lori.txt" in C:
+file2 = open(r"C:\\Users\\lorif\\Demo\\python-challenge\\PyBank\\analysis\\Final_Lori.txt","w+")
+
+#write to txt file
+file2.write("Financial Analysis\n")
+file2.write("------------------------------\n")
+file2.write(f"Total Months: {len(total_months)}\n")
+file2.write(f"Total: ${sum(total_profit)}\n")
+file2.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change), 2)}\n")
+file2.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})\n")
+file2.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})\n")
+
